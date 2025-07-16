a7a28eb238a407e8fda21565bcb93e8c89289b1b
ra-code422 <rafiatousoumailahamidou@gmail.com>
2025-07-16 02:26:46 +0100

    Add files via upload

--- simple_calculator.sh
+++ simple_calculator.sh
@@ -1,7 +1,33 @@
 #!/bin/bash
+# Script d'une calculatrice simple
 
-echo "Entrez les nombres :"
-read a b
+# Demande à l'utilisateur d'entrer deux nombres et un opérateur
+echo "Entrez les nombres et l'opérateur "
+read a b op  # 'a' est le 1er nombre, 'b' le 2e, 'op' l'opérateur (+ - × ÷)
 
-somme=$((a + b))
-echo "Résultat : $somme"
+# Vérifie si l'opérateur est une addition
+if [ "$op" = "+" ]; then
+   echo "Résultat : $((a + b))"  # Addition
+
+# Sinon si l'opérateur est une soustration
+elif [ "$op" = "-" ]; then
+   echo "Résultat : $((a - b))"  # Soustraction
+
+# Sinon si l'opérateur est une multiplication 
+elif [ "$op" = "*" ]; then
+   echo "Résultat : $((a * b))" #multiplication 
+
+# Sinon si l'opérateur est une division
+elif [ "$op" = "/" ]; then
+   # Vérifie si le 2e nombre est 0 (division interdite)
+   if [ "$b" -eq 0 ]; then
+      echo "Erreur : division par zéro"  # Message d'erreur
+   else
+      echo "Résultat : $((a / b))" # division
+   fi
+
+# Si l'opérateur n’est aucun de ceux reconnus
+else
+   echo "Résultat : opération invalide"
+fi
+
